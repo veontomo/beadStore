59afa4f0a26c221e58f7fb1d26ab593c7d550cda
Mario Rossi <veontomo@gmail.com>
2015-03-23 10:40:23 +0100

    feat: add a style file

--- beadStore/gen/com/veontomo/beadstore/R.java
+++ beadStore/gen/com/veontomo/beadstore/R.java
@@ -53,11 +53,13 @@ public final class R {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -76,5 +78,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int titleLine=0x7f060002;
     }
 }
