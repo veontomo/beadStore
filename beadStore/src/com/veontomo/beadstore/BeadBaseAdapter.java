aee40253184e11a0b218010c53b14f9e2ad5d174
Mario Rossi <veontomo@gmail.com>
2015-04-07 21:57:06 +0200

    feat: stub the get view method of BeadBaseAdapter

--- beadStore/src/com/veontomo/beadstore/BeadBaseAdapter.java
+++ beadStore/src/com/veontomo/beadstore/BeadBaseAdapter.java
@@ -16,7 +16,27 @@ import android.widget.BaseAdapter;
 public class BeadBaseAdapter extends BaseAdapter {
 	private Context mContext;
 	private ArrayList<BeadInfo> mData;
+	
+	/**
+	 * Number of kinds of layout for bead visualization
+	 * @since 0.6
+	 */
+	private final static int NUM_OF_TYPES = 2;
+	
+	/**
+	 * Layout with textual information
+	 * @since 0.6
+	 */
+	private final static int LAYOUT_TEXT = R.layout.bead_text;
 
+
+	/**
+	 * Layout with graphical information
+	 * @since 0.6
+	 */
+	private final static int LAYOUT_THUMBNAIL = R.layout.bead_thumbnail;
+
+	
 	public BeadBaseAdapter(Context context, ArrayList<BeadInfo> data) {
 		mContext = context;
 		this.mData = data;
@@ -35,6 +55,11 @@ public class BeadBaseAdapter extends BaseAdapter {
 	@Override
 	public long getItemId(int position) {
 		return position;
+	}
+
+	@Override
+	public int getViewTypeCount(){
+		return NUM_OF_TYPES;
 	}
 	
 	@Override  
